4b965566d9a7708cf618e8464ca394e00d02e440
chaospredictor <kuzi81@gmail.com>
2017-01-26 20:32:01 +0200

    working mark by click

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
-	
+	$(".place").click( function() {
+		buttonClicked(this);
+		//console.log("clicked");
+	});
 	
 });
+
+
+function buttonClicked(btn) {
+	//console.log(btn.id);
+	addObject("x", btn.id);
+}
+
+function addObject(obj, cltion) {
+	console.log("#"+cltion);
+	$("#"+cltion).text("X");
+}
