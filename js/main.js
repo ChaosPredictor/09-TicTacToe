426760403486fb28aedce080f2528bd11ee79cd6
chaospredictor <kuzi81@gmail.com>
2017-01-26 23:46:58 +0200

    working switch between users

--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
+var activeUser = 0;
+
 $(document).ready(function(){
 	$(".place").click( function() {
 		buttonClicked(this);
+		switchUser();
 		//console.log("clicked");
 	});
 	
 });
 
 
+function switchUser(){
+	console.log("switch user");
+	if (activeUser == 0){
+		activeUser = 1;
+	} else {
+		activeUser = 0;
+	}
+}
+
+function userToSign(user) {
+	if (activeUser == 0){
+		return "X";
+	} else {
+		return "O";
+	}
+}
+
 function buttonClicked(btn) {
 	//console.log(btn.id);
-	addObject("x", btn.id);
+	addObject(userToSign(activeUser), btn.id);
 }
 
 function addObject(obj, cltion) {
 	console.log("#"+cltion);
-	$("#"+cltion).text("X");
+	$("#"+cltion).text(obj);
 }
