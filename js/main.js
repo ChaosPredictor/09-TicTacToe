5dc1c0385b479f1732d8463dd0bcd9d1bd0464cd
chaospredictor <kuzi81@gmail.com>
2017-01-27 03:31:27 +0200

    check if user0/1 win

--- js/main.js
+++ js/main.js
@@ -33,14 +33,42 @@ function showActiveUser(){
 	$(".top-box").text("active user: " + activeUser);
 }
 
-function buttonClicked(btn) {
-	console.log(btn.attr('id'));
+function buttonClicked(td) {
+	console.log(td.attr('id'));
 	//$(this).removeClass("placeEmpty");
-	btn.find(".placeEmpty").remove();
-	addObject(userToSign(activeUser), btn.attr('id'));
+	td.find(".placeEmpty").remove();
+	td.addClass("place" + activeUser);
+	addObject(userToSign(activeUser), td.attr('id'));
+	if (checkIfWin(activeUser)) {
+		console.log("win");	
+	};
 }
 
 function addObject(obj, cltion) {
 	console.log("#"+cltion);
 	$("#"+cltion).append(obj);
+}
+
+function checkIfWin(user){
+	for(var i = 1; i <= 3; i++){
+		if($("#1"+i).hasClass("place"+user) && $("#2"+i).hasClass("place"+user) && $("#3"+i).hasClass("place"+user)) {
+			return true;
+			//console.log("win");
+		}
+	}
+	for(var i = 1; i <= 3; i++){
+		if($("#"+i+"1").hasClass("place"+user) && $("#"+i+"2").hasClass("place"+user) && $("#"+i+"3").hasClass("place"+user)) {
+			return true;
+			//console.log("win");
+		}
+	}
+	if ($("#13").hasClass("place"+user) && $("#22").hasClass("place"+user) && $("#31").hasClass("place"+user)) {
+		return true;
+		//console.log("win");
+	}
+	if ($("#11").hasClass("place"+user) && $("#22").hasClass("place"+user) && $("#33").hasClass("place"+user)) {
+		return true;
+		//console.log("win");
+	}
+	return false;
 }
